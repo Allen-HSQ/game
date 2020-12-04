789a31dd5af451284cb206c737e049276a2efdb9
hsq <1319185541@qq.com>
2020-12-04 16:40:06 +0800

    tijiao

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyAI : MonoBehaviour {
+    private NavMeshAgent agent;
+    private Vector3 target;
+    private Animator ani;
+	// Use this for initialization
+	void Start () {
+        ani = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        ani.SetBool("Run", true);
+        target = GameObject.FindGameObjectWithTag("Finish").transform.position;
+        target.x = Random.Range(-10f, 10f);
+        agent.destination = target;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(transform.position, target) > 0.5f)
+        {
+            ani.SetBool("Run", true);
+        }
+        else
+        {
+            ani.SetBool("Run", false);
+        }
+	}
+}
---
+++ Assets/Scripts/GameManage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManage : MonoBehaviour
+{
+    private GameObject[] enemys;
+    void Start()
+    {
+        enemys = Resources.LoadAll<GameObject>("Enemys");
+        Create();
+
+    }
+    void Update()
+    {
+
+    }
+
+    private void Create()
+    {
+        for (int i = -5; i < 5; i++)
+        {
+            GameObject enemy = Instantiate(enemys[Random.Range(0, enemys.Length)],
+                new Vector3(i * 2, 0, 0), Quaternion.identity);
+            enemy.AddComponent<EnemyAI>();
+        }
+    }
+}
